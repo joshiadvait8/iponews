24e759d9619af563e984c8407aaf829398e469f7
joshiadvait8 <joshiadvait8@gmail.com>
2020-10-03 20:22:02 +0530

    Updates

---
+++ precache-manifest.e48733d9aa435a8ce22ed4d816bf50e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eedb0ffa75e3e8baa5e696d2f7ebee0",
+    "url": "/iponews/index.html"
+  },
+  {
+    "revision": "f6e660157e8cf1623ae8",
+    "url": "/iponews/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b282d9d1989c61563293",
+    "url": "/iponews/static/css/main.053cef54.chunk.css"
+  },
+  {
+    "revision": "f6e660157e8cf1623ae8",
+    "url": "/iponews/static/js/2.490c1b7c.chunk.js"
+  },
+  {
+    "revision": "d714a5eb76ec3bd6395eaf46954b9bb3",
+    "url": "/iponews/static/js/2.490c1b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b282d9d1989c61563293",
+    "url": "/iponews/static/js/main.8e7cc22c.chunk.js"
+  },
+  {
+    "revision": "3c75bfcc3a4a2ae0297f",
+    "url": "/iponews/static/js/runtime-main.6e7703a6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d9e35979c6c7507b758554e924bebc8.js"
+  "/iponews/precache-manifest.e48733d9aa435a8ce22ed4d816bf50e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iponews/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
