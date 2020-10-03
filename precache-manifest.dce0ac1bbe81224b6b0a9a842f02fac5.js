35f7c7cda7589426909493b8ebc6a72dc4ba2347
joshiadvait8 <joshiadvait8@gmail.com>
2020-10-03 17:14:16 +0530

    Updates

---
+++ precache-manifest.dce0ac1bbe81224b6b0a9a842f02fac5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec0c7f28a745a343fdf62e16f6deb16",
+    "url": "/iponews/index.html"
+  },
+  {
+    "revision": "c8ae213ab82eccea0b1d",
+    "url": "/iponews/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "4daca012cac18e6b68f4",
+    "url": "/iponews/static/css/main.f2b800fc.chunk.css"
+  },
+  {
+    "revision": "c8ae213ab82eccea0b1d",
+    "url": "/iponews/static/js/2.edf2538c.chunk.js"
+  },
+  {
+    "revision": "d714a5eb76ec3bd6395eaf46954b9bb3",
+    "url": "/iponews/static/js/2.edf2538c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4daca012cac18e6b68f4",
+    "url": "/iponews/static/js/main.9e957548.chunk.js"
+  },
+  {
+    "revision": "3c75bfcc3a4a2ae0297f",
+    "url": "/iponews/static/js/runtime-main.6e7703a6.js"
+  }
+]);
